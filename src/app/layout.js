16c343958289cfd81c0b09f9bfbf46d0cd748971
saeed joulaei <saeedjoulaei86@gmail.com>
2024-08-04 19:47:23 +0330

    layout section added to project

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { yekan } from "@/utils/fonts";
 import "./globals.css";
+import Layout from "@/layout/Layout";
 
 export const metadata = {
   title: "املاک | پروژه sj-next",
@@ -10,7 +11,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
       <body className={yekan.className}>
-        <div>{children}</div>
+        <div>
+          <Layout>{children}</Layout>
+        </div>
       </body>
     </html>
   );
